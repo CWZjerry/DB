75226c74436663d0e98796d117adf6a9e406f69a
曹小泽的mac <caowenze@caowenzejiaowolaixunshan.local>
2016-12-30 09:45:39 +0800

    09.35

---
+++ DB-master/点吧/点吧/hoteTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  hoteTableViewCell.h
+//  点吧
+//
+//  Created by Jerry on 2016/12/29.
+//  Copyright © 2016年 OneGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hoteTableViewCell : UITableViewCell
+
+@end
